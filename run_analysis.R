f9af502dcc82317343e17c3ce7f53790c93fb13d
Abhimanyu <banerjee.abhimanyu3@gmail.com>
2016-12-28 14:22:10 -0500

    finish documentation for the R script

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,15 @@
 
 ## File Description:
 
-##
+## This script is a demonstration of a data cleaning pipeline. It works on the
+## datasets found in the UCI HAR Dataset and performs the following steps:
+## 1. Merge the training and the test sets to create one data set.
+## 2. Extract only the measurements on the mean and standard deviation for each
+##    measurement.
+## 3. Use descriptive activity names to name the activities in the data set
+## 4. Appropriately label the data set with descriptive variable names.
+## 5. From the data set in step 4, create a second, independent tidy data set
+##    with the average of each variable for each activity and each subject.
 ###############################################################################
 
 ## Setup
